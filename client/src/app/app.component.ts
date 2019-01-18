c80aa834b763618486243224a8e539dcfb8464b1
Lars Arvidsson <lars.arvidsson@cab.se>
2019-01-18 08:59:58 +0100

    Basic listing of todod

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { OktaAuthService } from '@okta/okta-angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent implements OnInit {
   isAuthenticated: boolean;
 
-  constructor(public oktaAuth: OktaAuthService) {
+  constructor(public oktaAuth: OktaAuthService, private router: Router) {
     // Subscribe to authentication state changes
     this.oktaAuth.$authenticationState.subscribe(
       (isAuthenticated: boolean) => (this.isAuthenticated = isAuthenticated)
@@ -19,10 +21,14 @@ export class AppComponent implements OnInit {
   async ngOnInit() {
     // Get the authentication state for immediate use
     this.isAuthenticated = await this.oktaAuth.isAuthenticated();
+
+    if (this.isAuthenticated) {
+      this.router.navigate(['/todos']);
+    }
   }
 
   login() {
-    this.oktaAuth.loginRedirect('/profile');
+    this.oktaAuth.loginRedirect('/todos');
   }
 
   logout() {
--- client/src/app/todos/todos.component.ts
+++ client/src/app/todos/todos.component.ts
@@ -1,19 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { Todo } from '../models/todo';
 import { TodosService } from '../todos.service';
 
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
-  styleUrls: ['./todos.component.scss']
+  styleUrls: ['./todos.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TodosComponent implements OnInit {
   todos: Todo[];
 
-  constructor(private todosService: TodosService) { }
+  constructor(private todosService: TodosService) {}
 
   ngOnInit() {
-    this.todosService.getTodos().subscribe(x => this.todos = x);
+    this.todosService.getTodos().subscribe(x => (this.todos = x));
   }
-
 }
