26b16593210167c166fa7b5632188d6c30540c61
Lars Arvidsson <lars.arvidsson@cab.se>
2019-01-18 10:16:58 +0100

    Updated todo model

--- server/src/todos/todos.controller.ts
+++ server/src/todos/todos.controller.ts
@@ -5,6 +5,10 @@ import { Todo } from 'src/models/todo';
 export class TodosController {
   @Get()
   getTodos(): Todo[] {
-    return [{ title: 'att göra 1' }, { title: 'att göra 2' }, { title: 'att göra 3' }];
+    return [
+      { id: 1, title: 'att göra 1', checked: false },
+      { id: 2, title: 'att göra 2', checked: false },
+      { id: 3, title: 'att göra 3', checked: false },
+    ];
   }
 }
